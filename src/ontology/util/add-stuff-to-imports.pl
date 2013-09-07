44482813aae8c04b1030c4fbb4e431c9f3254431
cmungall <cmungall@485a086a-56a8-a8b5-2d01-7d13b9f3bacb>
2013-09-07 01:01:23 +0000

    new release

--- src/ontology/util/add-stuff-to-imports.pl
+++ src/ontology/util/add-stuff-to-imports.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/perl
-my $OBO=http://purl.obolibrary.org/obo
+my $OBO="http://purl.obolibrary.org/obo";
 my $ont = shift @ARGV;
-my $cmd = "owltools imports/$ont"."_import.owl $OBO/$ont".".owl --extract-module -s $OBO/$ont".".owl '@ARGV' --merge-support-ontologies -o imports/$ont"."_import-staged.owl";
-print `$cmd`;
+my $import_ont = "imports/$ont"."_import.owl";
+my $import_ont_staged = "imports/$ont"."_import-staged.owl";
+my $owl = "$OBO/$ont.owl";
+
+#my $cmd = "owltools $import_ont $owl --extract-module -s $owl @ARGV --merge-support-ontologies --extract-mingraph -o $import_ont_staged";
+runcmd("owltools  $owl --extract-module -s $owl @ARGV --extract-mingraph -o $import_ont_staged");
+runcmd("owltools $import_ont $import_ont_staged --merge-support-ontologies -o $import_ont_staged");
+exit 0;
+
+sub runcmd {
+    my $cmd = shift;
+    print STDERR "CMD = $cmd\n";
+    if (system($cmd)) 
+        {
+            die "Cannot execute $cmd";
+        }
+}
+
