e5038b8a3df5d3c7fdcf5d267b82d6faccd1742e
Ugur Bayindir <ubyndr@gmail.com>
2023-10-12 17:07:45 +0100

    Refactored generic_coverage.py to work on local copy of cl-base.owl

--- src/scripts/generic_coverage.py
+++ src/scripts/generic_coverage.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 import argparse
 import csv
-import pandas as pd
-from SPARQLWrapper import SPARQLWrapper, JSON
-from typing import Dict, List
+import os
+import subprocess
+from typing import Any, Dict, List, Union
 
-UBERGRAPH_ENDPOINT = "https://ubergraph.apps.renci.org/sparql"
+from rdflib import Graph
 
 
-def calculate_coverage(_scope_dict: Dict[str, str], _term_leaves_dict: Dict[str, Dict[str, str]]):
+def calculate_coverage(
+    _scope_dict: Dict[str, str], _term_leaves_dict: Dict[str, Dict[str, str]]
+):
     """Calculates coverage and returns list that contains coverage status of terms under the scope. Also returns
     not covered term list and a report that shows the percentage of the coverage
 
@@ -19,11 +21,28 @@ def calculate_coverage(_scope_dict: Dict[str, str], _term_leaves_dict: Dict[str,
     Returns:
         * A coverage report with number of total terms and percentage of the covered terms
     """
-    _covered_term_count_by_each_term = [[term, len(scope_members)] for term, scope_members in _term_leaves_dict.items()]
+    _covered_term_count_by_each_term = [
+        [term, len(scope_members)] for term, scope_members in _term_leaves_dict.items()
+    ]
     sort_term_count = sorted(_covered_term_count_by_each_term, key=lambda x: x[1])
-    covered_term = set([member for scope_members in _term_leaves_dict.values() for member in scope_members if member in _scope_dict.keys()])
-    _not_covered_list = [[scope_iri, scope_label] for scope_iri, scope_label in _scope_dict.items() if scope_iri not in covered_term]
-    return f"{100 * (len(covered_term) / len(_scope_dict)):.2f}%", sort_term_count, _not_covered_list
+    covered_term = set(
+        [
+            member
+            for scope_members in _term_leaves_dict.values()
+            for member in scope_members
+            if member in _scope_dict.keys()
+        ]
+    )
+    _not_covered_list = [
+        [scope_iri, scope_label]
+        for scope_iri, scope_label in _scope_dict.items()
+        if scope_iri not in covered_term
+    ]
+    return (
+        f"{100 * (len(covered_term) / len(_scope_dict)):.2f}%",
+        sort_term_count,
+        _not_covered_list,
+    )
 
 
 def generate_scope_dict(_term_dict: Dict[str, str], _scope: str) -> Dict[str, str]:
@@ -35,46 +54,56 @@ def generate_scope_dict(_term_dict: Dict[str, str], _scope: str) -> Dict[str, st
 
 def get_scope_terms(_scope: str) -> Dict[str, str]:
     # Get terms under the scope with IRIs and labels from Ubergraph
-    sparql.setQuery(get_scope_query(_scope))
-    scope_query_response = sparql.queryAndConvert()
+    query = get_scope_query(_scope)
     _scope_dict: Dict[str, str] = {}
-    for item in scope_query_response["results"]["bindings"]:
-        _scope_dict.update({item['scope_member']['value']: item['label']['value']})
+
+    for _result in cl.query(query):
+        _scope_dict.update({_result.scope_member.toPython(): _result.label.toPython()})
     return _scope_dict
 
 
-def clean_up_scope_terms(_term_dict: Dict[str, str], _scope_dict: Dict[str, str], _scope: str) -> Dict[str, str]:
+def clean_up_scope_terms(
+    _term_dict: Dict[str, str], _scope_dict: Dict[str, str], _scope: str
+) -> Dict[str, str]:
     # Remove all superclasses of terms on the scope list (via subClassOf/ and part_of)
-    sparql.setQuery(get_superclass_value_query(list(_term_dict.values()), _scope))
-    super_ret = sparql.queryAndConvert()
-    for super_class in super_ret["results"]["bindings"]:
-        _scope_dict.pop(super_class['super']['value'], None)
+    query = get_superclass_value_query(list(_term_dict.values()), _scope)
+
+    for _result in cl.query(query):
+        _scope_dict.pop(_result.super.toPython(), None)
     return _scope_dict
 
 
 def get_term_leaves(term_list: List[str], _scope: str) -> Dict[str, Dict[str, str]]:
     # Get terms under, connected with 'subClassOf' relation, given the term list via template file from Ubergraph
+    # Prepare the SPARQL query
+    query = get_term_leaves_list_query(term_list, _scope)
+
     _term_leaves_dict = {}
-    sparql.setQuery(get_term_leaves_list_query(term_list, _scope))
-    ret = sparql.queryAndConvert()
-    for row in ret["results"]["bindings"]:
-        if row['term_label']['value'] not in _term_leaves_dict.keys():
-            _term_leaves_dict.update(
-                {row['term_label']['value']: {row['term_leaf']['value']: row['term_leaf_label']['value']}})
+    for _result in cl.query(query):
+        term_label = _result.term_label.toPython()
+        term_leaf = _result.term_leaf.toPython()
+        term_leaf_label = _result.term_leaf_label.toPython()
+
+        if term_label not in _term_leaves_dict:
+            _term_leaves_dict[term_label] = {term_leaf: term_leaf_label}
         else:
-            _term_leaves_dict[row['term_label']['value']].update(
-                {row['term_leaf']['value']: row['term_leaf_label']['value']})
+            _term_leaves_dict[term_label][term_leaf] = term_leaf_label
+
     return _term_leaves_dict
 
 
-def get_invalid_subclass_list(term_list: List[str]) -> List[str]:
+def get_invalid_subclass_list(term_list: List[str]) -> list[list[Union[str, Any]]]:
     # Get slim terms that are subclasses of other slim terms
-    invalid_subclass_list = []
-    sparql.setQuery(get_invalid_subclass_list_query(term_list))
-    ret = sparql.queryAndConvert()
-    for row in ret["results"]["bindings"]:
-        invalid_subclass_list.append([row["sub"]["value"] + " " + row["sub_label"]["value"], "rdfs:subClassOf", row["obj"]["value"] + " " + row["obj_label"]["value"]])
-    return invalid_subclass_list
+    query = get_invalid_subclass_list_query(term_list)
+
+    return [
+        [
+            f"{_result.sub.toPython()} {_result.sub_label.toPython()}",
+            "rdfs:subClassOf",
+            f"{_result.obj.toPython()} {_result.obj_label.toPython()}",
+        ]
+        for _result in cl.query(query)
+    ]
 
 
 def get_scope_query(scope_term: str) -> str:
@@ -92,11 +121,12 @@ def get_scope_query(scope_term: str) -> str:
         PREFIX owl: <http://www.w3.org/2002/07/owl#>
         PREFIX UBERON: <http://purl.obolibrary.org/obo/UBERON_>
         PREFIX CL: <http://purl.obolibrary.org/obo/CL_>
+        PREFIX BFO: <http://purl.obolibrary.org/obo/BFO_>
+        PREFIX RO: <http://purl.obolibrary.org/obo/RO_>
         SELECT ?scope_member ?label
         WHERE
         {{
-          ?scope_member <http://www.w3.org/2000/01/rdf-schema#subClassOf>|<http://purl.obolibrary.org/obo/BFO_0000050> {_scope} .
-          ?scope_member rdfs:isDefinedBy <http://purl.obolibrary.org/obo/cl.owl> .
+          ?scope_member rdfs:subClassOf|BFO:0000050|RO:0002100 {_scope} .
           ?scope_member rdfs:label ?label. 
         }}
     """
@@ -116,14 +146,14 @@ def get_superclass_value_query(term_iri_list: List[str], _scope: str) -> str:
             PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
             PREFIX UBERON: <http://purl.obolibrary.org/obo/UBERON_>
             PREFIX CL: <http://purl.obolibrary.org/obo/CL_>
+            PREFIX BFO: <http://purl.obolibrary.org/obo/BFO_>
             SELECT DISTINCT ?super ?label
             WHERE
             {{
-              ?term <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?super. ?super rdfs:label ?label. 
-              ?super <http://www.w3.org/2000/01/rdf-schema#subClassOf>|<http://purl.obolibrary.org/obo/BFO_0000050> {_scope}.
-              ?super rdfs:isDefinedBy <http://purl.obolibrary.org/obo/cl.owl> .
+              ?term rdfs:subClassOf ?super. ?super rdfs:label ?label. 
+              ?super rdfs:subClassOf|BFO:0000050 {_scope}.
               VALUES ?term {{{' '.join(term_iri_list)}}}
-            FILTER(?term != ?super)
+            FILTER(?term != ?super)      
             }}
         """
 
@@ -143,12 +173,13 @@ def get_term_leaves_list_query(term_iri_list: List[str], scope_term: str) -> str
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         PREFIX CL: <http://purl.obolibrary.org/obo/CL_>
         PREFIX UBERON: <http://purl.obolibrary.org/obo/UBERON_>
+        PREFIX BFO: <http://purl.obolibrary.org/obo/BFO_>
         SELECT ?term_label ?term_leaf ?term_leaf_label
         WHERE
         {{
-          ?term_leaf <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?term.
-          ?term_leaf rdfs:label ?term_leaf_label. ?term_leaf rdfs:isDefinedBy <http://purl.obolibrary.org/obo/cl.owl> .
-          ?term_leaf <http://www.w3.org/2000/01/rdf-schema#subClassOf>|<http://purl.obolibrary.org/obo/BFO_0000050> {_scope}. 
+          ?term_leaf rdfs:subClassOf ?term.
+          ?term_leaf rdfs:label ?term_leaf_label.
+          ?term_leaf rdfs:subClassOf|BFO:0000050 {_scope}. 
           ?term rdfs:label ?term_label.
           VALUES ?term {{{' '.join(term_iri_list)}}}
         }}
@@ -179,43 +210,126 @@ def get_invalid_subclass_list_query(term_iri_list: List[str]) -> str:
     """
 
 
-if __name__ == '__main__':
+def run_command(command, wd):
+    try:
+        completed_process = subprocess.run(
+            command,
+            cwd=wd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True,
+        )
+        print(f"{command[0]} command executed successfully")
+        print("Standard Output:")
+        print(completed_process.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running {command[0]} command: {e}")
+        print("Standard Error:")
+        print(e.stderr)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--scope', help='''Upper class term that you want to calculate coverage of your slim''')
-    parser.add_argument('-f', '--file', help='''File path of your slim's template file''')
-    parser.add_argument('-o', '--output', help='''Output file name''')
+    parser.add_argument(
+        "-s",
+        "--scope",
+        help="""Upper class term that you want to calculate coverage of your slim""",
+    )
+    parser.add_argument(
+        "-f", "--file", help="""File path of your slim's template file"""
+    )
+    parser.add_argument("-o", "--output", help="""Output file name""")
     args = parser.parse_args()
 
     file_name = str(args.file)
     scope = str(args.scope)
     output_file = str(args.output)
 
-    # SPARQLWrapper init
-    sparql = SPARQLWrapper(UBERGRAPH_ENDPOINT)
-    sparql.setReturnFormat(JSON)
+    # Change -ti flag to -t in the run.sh
+    docker_script = "../ontology/run.sh"
+    docker_script_temp = "../ontology/run_temp.sh"
 
-    term_dict = pd.read_csv(file_name, usecols=["ID", "label"], index_col=1).iloc[1:, :].squeeze().to_dict()
+    # Read the content of the input file
+    with open(docker_script, "r") as f:
+        lines = f.readlines()
+
+    # Make the change
+    if len(lines) >= 70:
+        lines[69] = lines[69].replace("-ti", "-t")
+
+    # Write the modified content to the output file
+    with open(docker_script_temp, "w") as f:
+        f.writelines(lines)
+
+    # Define the working directory where you want to run the command
+    working_directory = "../ontology/"
+    make_command = [
+        "sh",
+        "run_temp.sh",
+        "make",
+        "cl-base.owl",
+        "MIR=false",
+        "IMP=false",
+    ]
+    relation_graph_command = [
+        "relation-graph",
+        "--ontology-file",
+        "cl-base.owl",
+        "--output-file",
+        "test.ttl",
+        "--output-subclasses",
+        "true",
+        "--reflexive-subclasses",
+        "false",
+        "--property",
+        "http://purl.obolibrary.org/obo/BFO_0000050",
+        "--property",
+        "http://purl.obolibrary.org/obo/RO_0002100",
+    ]
+
+    run_command(make_command, working_directory)
+    os.remove(docker_script_temp)
+    run_command(relation_graph_command, working_directory)
+
+    cl_base = Graph().parse("../ontology/cl-base.owl", format="xml")
+    cl_rel = Graph().parse("../ontology/test.ttl", format="ttl")
+    cl = cl_base + cl_rel
+
+    term_dict = {}
+    with open(file_name, mode="r", encoding="utf-8-sig", newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        # Skip the first line
+        next(reader)
+        for _row in reader:
+            term_dict[_row["label"]] = _row["ID"]
 
     term_leaves_dict = get_term_leaves(list(term_dict.values()), scope)
     invalid_slim_term_list = get_invalid_subclass_list(list(term_dict.values()))
     if invalid_slim_term_list:
-        invalid_report_file = output_file.replace("reports/", "reports/overlapping_terms_", 1)
-        with open(invalid_report_file, 'w+', newline='') as invalid_file:
+        invalid_report_file = output_file.replace(
+            "reports/", "reports/overlapping_terms_", 1
+        )
+        with open(invalid_report_file, "w+", newline="") as invalid_file:
             write = csv.writer(invalid_file)
             write.writerows(invalid_slim_term_list)
         # Disabling the exception for now
         # raise Exception(f"{file_name} is invalid! {invalid_report_file} report for more details")
     scope_dict = generate_scope_dict(term_dict, scope)
-    report_str,  covered_term_count_by_each_term, not_covered_list = calculate_coverage(scope_dict, term_leaves_dict)
+    report_str, covered_term_count_by_each_term, not_covered_list = calculate_coverage(
+        scope_dict, term_leaves_dict
+    )
     result = list()
     result.append(["#####Coverage percentage#####"])
     result.append([report_str])
     result.append(["#####Number of terms covered by each term in the slim#####"])
     result.extend(covered_term_count_by_each_term)
-    result.append([f"#####Terms that are not covered by {file_name} under {scope}#####"])
+    result.append(
+        [f"#####Terms that are not covered by {file_name} under {scope}#####"]
+    )
     result.extend(not_covered_list)
     if output_file:
-        with open(output_file, 'w+', newline='') as file:
+        with open(output_file, "w+", newline="") as file:
             write = csv.writer(file)
             write.writerows(result)
     print(f"{file_name} has {report_str} coverage over {scope}")
