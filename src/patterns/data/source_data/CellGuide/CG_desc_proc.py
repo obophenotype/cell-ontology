ef21a4c6f1d06f35d820487ffaeb7aae6d8dbd34
dosumis <dosumis@gmail.com>
2024-08-30 10:42:02 +0100

    Adding validated, approved CellGuide patterns
    Fixes #2081 #2350

---
+++ src/patterns/data/source_data/CellGuide/CG_desc_proc.py
@@ -0,0 +1,20 @@
+import pandas as pd
+cg_desc = pd.read_excel('./CellGuide Validated Descriptions for CL Review.xlsx')
+cg_desc.fillna('', inplace=True)
+rows = []
+for i,r in cg_desc.iterrows():
+    if not (r['For CL inclusion'] == 1):
+        next
+    row = {'defined_class': r['CL ID'], 'CL_short_form': str(r['CL ID']).replace(':', '_'),
+           'desc': r["Final version (QC'd)"], 'pubs': ''}
+    pub_list = []
+    for k,v in r.items():
+            if str(k).startswith('Supporting'):
+               if str(v).startswith('http'):
+                   pub_list.append(v)
+               elif(v):
+                   pub_list.append('DOI:' + v)
+    row['pubs'] = '|'.join(pub_list)
+    rows.append(row)
+out = pd.DataFrame.from_records(rows)
+out.to_csv('../../default/ExtendedDescription.tsv', sep='\t')
