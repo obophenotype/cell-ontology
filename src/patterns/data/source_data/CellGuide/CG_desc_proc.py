4b18655d33bad18b05131b6651aa9b5a5d0f107e
dosumis <dosumis@gmail.com>
2024-09-06 17:16:17 +0100

    Updated CellGuide TSV + script to generate from xlsx sheet

--- src/patterns/data/source_data/CellGuide/CG_desc_proc.py
+++ src/patterns/data/source_data/CellGuide/CG_desc_proc.py
@@ -2,9 +2,13 @@ import pandas as pd
 cg_desc = pd.read_excel('./CellGuide Validated Descriptions for CL Review.xlsx')
 cg_desc.fillna('', inplace=True)
 rows = []
+rejects = 0
 for i,r in cg_desc.iterrows():
-    if not (r['For CL inclusion'] == 1):
-        next
+    print(bool(r['For CL inclusion']))
+    if not (r['For CL inclusion']):
+        if r['For CL inclusion'] == 0:
+            rejects += 1
+        continue
     row = {'defined_class': r['CL ID'], 'CL_short_form': str(r['CL ID']).replace(':', '_'),
            'desc': r["Final version (QC'd)"], 'pubs': ''}
     pub_list = []
@@ -16,5 +20,7 @@ for i,r in cg_desc.iterrows():
                    pub_list.append('DOI:' + v)
     row['pubs'] = '|'.join(pub_list)
     rows.append(row)
+print("Added %d extended defs" % len(rows))
+print("Rejected descriptions %d" % rejects)
 out = pd.DataFrame.from_records(rows)
 out.to_csv('../../default/ExtendedDescription.tsv', sep='\t', index=False)
