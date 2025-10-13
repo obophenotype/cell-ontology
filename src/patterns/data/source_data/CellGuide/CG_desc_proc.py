af1803686ae713fa66b9a4819362c665c3f45c9f
CEastwood <116108977+Caroline-99@users.noreply.github.com>
2025-10-13 10:42:00 +0100

    Adding approved CellGuide defs (#3378)
    * Adding approved CellGuide defs
    
    Uploaded the updated CellGuide excel sheet
    Generated the ExtendedDescription.tsv
    Updated definition.owl
    
    * updated the python script to include cell = CL:ID
    
    row = {
            "defined_class": r["CL ID"],
            "cell": r["CL ID"],
            "CL_short_form": str(r["CL ID"]).replace(":", "_"),
            "desc": r["Final version (QC'd)"],
            "pubs": "",
        }

--- src/patterns/data/source_data/CellGuide/CG_desc_proc.py
+++ src/patterns/data/source_data/CellGuide/CG_desc_proc.py
@@ -1,26 +1,32 @@
 import pandas as pd
-cg_desc = pd.read_excel('./CellGuide Validated Descriptions for CL Review.xlsx')
-cg_desc.fillna('', inplace=True)
+
+cg_desc = pd.read_excel("./CellGuide Validated Descriptions for CL Review.xlsx")
+cg_desc.fillna("", inplace=True)
 rows = []
 rejects = 0
-for i,r in cg_desc.iterrows():
-    print(bool(r['For CL inclusion']))
-    if not (r['For CL inclusion']):
-        if r['For CL inclusion'] == 0:
+for i, r in cg_desc.iterrows():
+    print(bool(r["For CL inclusion"]))
+    if not (r["For CL inclusion"]):
+        if r["For CL inclusion"] == 0:
             rejects += 1
         continue
-    row = {'defined_class': r['CL ID'], 'CL_short_form': str(r['CL ID']).replace(':', '_'),
-           'desc': r["Final version (QC'd)"], 'pubs': ''}
+    row = {
+        "defined_class": r["CL ID"],
+        "cell": r["CL ID"],
+        "CL_short_form": str(r["CL ID"]).replace(":", "_"),
+        "desc": r["Final version (QC'd)"],
+        "pubs": "",
+    }
     pub_list = []
-    for k,v in r.items():
-            if str(k).startswith('Supporting'):
-               if str(v).startswith('http'):
-                   pub_list.append(v)
-               elif(v):
-                   pub_list.append('DOI:' + v)
-    row['pubs'] = '|'.join(pub_list)
+    for k, v in r.items():
+        if str(k).startswith("Supporting"):
+            if str(v).startswith("http"):
+                pub_list.append(v)
+            elif v:
+                pub_list.append("DOI:" + v)
+    row["pubs"] = "|".join(pub_list)
     rows.append(row)
 print("Added %d extended defs" % len(rows))
 print("Rejected descriptions %d" % rejects)
 out = pd.DataFrame.from_records(rows)
-out.to_csv('../../default/ExtendedDescription.tsv', sep='\t', index=False)
+out.to_csv("../../default/ExtendedDescription.tsv", sep="\t", index=False)
